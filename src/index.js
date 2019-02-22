0bea56d8149e8bf5b82c7bba4c72f6743c162657
Ahn sena <serendipity.sena@gmail.com>
2019-02-22 16:25:37 +0900

    modified css

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// const introduceEl = document.querySelector(".introduce");
+// const titleEl = document.querySelector("#introduce--title");
+
+// titleEl.addEventListener("click", function() {
+//   introduceEl.classList.toggle("act");
+// });
